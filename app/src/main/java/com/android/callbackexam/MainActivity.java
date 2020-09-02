ab3fbd05fd5110cbb3a0661bfa77e813b83fd6ee
Seongjunsim <sjsim0319@gmail.com>
2020-09-02 17:56:35 +0900

    good

--- app/src/main/java/com/android/callbackexam/MainActivity.java
+++ app/src/main/java/com/android/callbackexam/MainActivity.java
@@ -1,13 +1,22 @@
 package com.android.callbackexam;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ColorListFragment.OnColorSelectedListener{
 
+    private ColorFragment mColorFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mColorFragment = (ColorFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_color);
+
+    }
+
+    @Override
+    public void onColorSelected(int color){
+        mColorFragment.setColor(color);
     }
 }
